b1620b0c6fc13a8b0cb8ea1262524d31d0e9cb1c
sagarshakya45 <sagar.shakya2024@lpu.in>
2026-09-14 00:25:35 +0530

    Connect frontend to deployed backend

--- frontend/src/services/apiClient.ts
+++ frontend/src/services/apiClient.ts
@@ -1,7 +1,7 @@
 import axios, { AxiosError, InternalAxiosRequestConfig, AxiosResponse } from 'axios';
 
-const baseURL = import.meta.env.VITE_API_URL || 'http://localhost:5000/api';
-
+// const baseURL = import.meta.env.VITE_API_URL;
+const baseURL = `${import.meta.env.VITE_API_URL}/api`;
 export const apiClient = axios.create({
   baseURL,
   withCredentials: true,
